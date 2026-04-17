b3ecb2014ecba33627a91a1045b4eefd67c4f416
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-04-17 12:44:00 +0200

    added Tailwind CSS configuration for theme customization for colors, and font

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        background: "#27282B",
+        surface: "#201E1E",
+        accent: "#55EC20",
+        text: "#EBEBEB",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+};
